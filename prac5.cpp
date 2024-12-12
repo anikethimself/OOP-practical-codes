dcb107759553f2dd395c421adcbf07e032eb668d
Aniket Kamble <aktitanium8055@gmail.com>
2024-12-12 21:50:50 +0530

    Update prac5.cpp

--- prac5.cpp
+++ prac5.cpp
@@ -1,55 +1,53 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-template <typename T>
-void selectionSort(T arr[], int size) {
-    for (int i = 0; i < size - 1; ++i) {
-        int minIndex = i;
-        for (int j = i + 1; j < size; ++j) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
+template<typename T>
+void selection_sort(T arr[],int size){
+    for(int i=0; i<size-1; ++i){
+        int minterm=i;
+        for(int j=i+1; j<size; ++j){
+            if(arr[j]<arr[minterm]){
+                minterm=j;
             }
         }
-
-        T temp = arr[i];
-        arr[i] = arr[minIndex];
-        arr[minIndex] = temp;
+        T temp= arr[i];
+        arr[i]= arr[minterm];
+        arr[minterm]= temp;
     }
 }
 
-
-template <typename T>
-void inputArray(T arr[], int size) {
-    cout << "Enter " << size << " elements:" << endl;
-    for (int i = 0; i < size; ++i) {
-        cin >> arr[i];
+template<typename T>
+void getdata(T arr[], int size){
+    cout<<"Enter the "<<size<<" Elements:\n";
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
     }
+    
 }
 
-template <typename T>
-void outputArray(const T arr[], int size) {
-    for (int i = 0; i < size; ++i) {
-        cout << arr[i] << " ";
+template<typename T>
+void printdata(T arr[], int size) const{
+    cout<<"the elements are:";
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
     }
-    cout << endl;
+    cout<<endl;
 }
-
-int main() {
-    // integer array
-    int intArr[5];
-    cout << "Input for integer array:" << endl;
-    inputArray(intArr, 5);
-    selectionSort(intArr, 5);
-    cout << "Sorted integer array: ";
-    outputArray(intArr, 5);
-
-    // float array
-    float floatArr[5];
-    cout << "Input for float array:" << endl;
-    inputArray(floatArr, 5);
-    selectionSort(floatArr, 5);
-    cout << "Sorted float array: ";
-    outputArray(floatArr, 5);
-
+int main(){
+    int intarr[5];
+    cout<<"input for integer array:"<<endl;
+    getdata(intarr,5);
+    selection_sort(intarr,5);
+    cout<<"sorted int array:"<<endl;
+    printdata(intarr,5);
+    cout<<""<<endl;
+    
+    float floatarr[5];
+    cout<<"input for float array:"<<endl;
+    getdata(floatarr,5);
+    selection_sort(floatarr,5);
+    cout<<"sorted float array:"<<endl;
+    printdata(floatarr,5);
+    
     return 0;
 }
